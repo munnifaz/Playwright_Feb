6936f2c6c1363c907e3d95275886827fe268ff88
munnifaz <75413774+munnifaz@users.noreply.github.com>
2024-02-24 19:08:03 +1100

    Add files via upload
    Please review the latest one.

--- Week1_HomeAssignments.js
+++ Week1_HomeAssignments.js
@@ -131,7 +131,7 @@ let chars=OrginalStr.split("");
  */
    // ******************************print between 1, 10 --> keep adding the sum of values********************************************************
 
-    /* function SumOfAllValues()
+    /*  function SumOfAllValues()
     {
         let val=7;
         i=1, j=2;k='';
@@ -147,4 +147,110 @@ let chars=OrginalStr.split("");
         
         }
 
-SumOfAllValues(); */
+SumOfAllValues();  */
+
+ //******************iven an integer array, find the maximum amd minimum elements in an array and return their indices. */
+
+  /* Arr1=[1,5,6,3,0,2,3,10,67,89,3,2,-1,-56,-98]
+
+ let sortedArr=Arr1.sort();
+
+ //console.log(sortedArr);
+
+ let Minimum=sortedArr[0];
+
+ findmax=sortedArr.length-1
+
+ let Maximum=sortedArr[findmax]
+
+ console.log(Minimum,Maximum);  */
+
+ //****************************************************
+
+ //************return duplicates*************************************
+
+  /* Arr1=[1,2,5,3,8,5,6,2,1]
+ sortedarr=Arr1.sort();
+ console.log(sortedarr);
+ uniquearray=[];
+ 
+
+ for (let i=0;i<=sortedarr.length;i++){
+    
+    for(let j=i+1;j<=sortedarr.length-1;j++)
+
+    {
+        if(sortedarr[i]==sortedarr[j]){
+           
+            uniquearray=uniquearray+sortedarr[i]       
+       }   
+    }
+ } 
+ console.log(uniquearray); */
+ 
+ 
+//*************************************************Occurances*****************************************
+
+/* Arr1=[1,5,3,8,6,2,2,2,2,2,2,2,2,2,2]
+sortedarr=Arr1.sort();
+console.log(sortedarr);
+let k=1;
+for (let i=0;i<=sortedarr.length-1;i++){
+   
+   j=i+1;
+   {     if(sortedarr[i]==sortedarr[j]){
+          
+           k=k+1;
+      }  
+   }
+} 
+console.log(k); 
+ */
+//*******************************************move zero's to the end************************ */
+/* 
+ var numArray = new Float64Array([0,1,0,3,12]);
+numArray = numArray.sort();
+console.log(numArray) 
+
+Arr1=[0,1,0,3,12,0,4,6];
+Arr2=[];
+Arr3=[];
+Finalarr=[];
+
+for (let i=0; i<=Arr1.length-1;i++){
+
+if(Arr1[i]==0){
+Arr2.push(Arr1[i])
+
+}
+else{
+    Arr3.push(Arr1[i]);
+
+}
+}
+console.log(Arr2,Arr3);
+Finalarr=Arr3.concat(Arr2);
+console.log(Finalarr);  */
+
+//*************************************remove duplicates in an array******************************//
+const Arr1 = [1, 2, 5, 3, 8, 5, 6, 2, 1];
+const sortedarr = Arr1.slice().sort(); // Make a copy of Arr1 before sorting
+console.log(sortedarr);
+const uniquearray = [];
+
+for (let i = 0; i < sortedarr.length; i++) {
+    let isDuplicate = false;
+    for (let j = i + 1; j < sortedarr.length; j++) {
+        if (sortedarr[i] === sortedarr[j]) {
+            isDuplicate = true;
+            break;
+        }
+    }
+    if (!isDuplicate) {
+        uniquearray.push(sortedarr[i]);
+    }
+}
+console.log(uniquearray);
+
+//*******Given two integer arrays nums1 and nums2, return an array of their intersection.
+// Each element in the result must be unique and you may return the result in any order.//********************* */
